482137fa79f0d8eb66a592bc26455935355963dd
Anton Evdakov <evdanton2000@gmail.com>
2020-02-23 20:22:22 +0300

    Create socket for server

--- .src/service.js
+++ .src/service.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const socketIO = require('socket.io')
 const path = require('path')
+const http = require('http')
 
 const publicPath = path.join(__dirname, '../public')
 
@@ -8,11 +10,16 @@ const port = process.env.PORT || 4200
 
 //instance - get results of express function
 const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
 
 //folder public now is static
 app.use(express.static(publicPath))
 
+io.on('connection', () => {
+    console.log('IO Connection')
+})
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`Server has been started on port ${port}...`)
 })
