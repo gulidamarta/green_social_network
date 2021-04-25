04dd99ba53de31370a8e753b73dc2815950e5c9f
Marta <43724620+gulidamarta@users.noreply.github.com>
2021-04-26 00:00:05 +0300

    Add maps functionality, with hitmap, pins

--- routes/activities.js
+++ routes/activities.js
@@ -13,85 +13,91 @@ let Activities = require('../models/activity');
 // Chat Model
 let Chats = require('../models/chat');
 
-// Add Submit POST Route
-router.post('/add', function (req, res) {
-    req.checkBody('name', 'Name is required.').notEmpty();
-    req.checkBody('author', 'Author is required.').notEmpty();
-    req.checkBody('latitude', 'Latitude is required.').notEmpty();
-    req.checkBody('longitude', 'Longitude is required.').notEmpty();
-    req.checkBody('start_date', 'Start Date is required.').notEmpty();
-    req.checkBody('end_date', 'End Date is required.').notEmpty();
+//
+// // Add Submit POST Route
+// router.post('/add', function (req, res) {
+//     req.checkBody('name', 'Name is required.').notEmpty();
+//     req.checkBody('author', 'Author is required.').notEmpty();
+//     req.checkBody('latitude', 'Latitude is required.').notEmpty();
+//     req.checkBody('longitude', 'Longitude is required.').notEmpty();
+//     req.checkBody('start_date', 'Start Date is required.').notEmpty();
+//     req.checkBody('end_date', 'End Date is required.').notEmpty();
+//
+//     // Get Errors
+//     let errors = req.validationErrors();
+//     if (errors){
+//         res.render('index', {
+//             title: 'Green News',
+//             errors: errors
+//         })
+//     }else{
+//         // TODO: add checking that end date is later than start date
+//         let activitySchedule = new ActivitySchedule();
+//         activitySchedule.start_date = req.body.start_date;
+//         activitySchedule.end_date = req.body.end_date;
+//
+//         activitySchedule.save(function (err){
+//             if (err){
+//                 console.log(err);
+//             }
+//         });
+//
+//         let activityPlace = new ActivityPlace();
+//         activityPlace.latitude = req.body.latitude;
+//         activityPlace.longitude = req.body.longitude;
+//
+//         activityPlace.save(function (err){
+//             if (err){
+//                 console.log(err);
+//             }
+//         });
+//
+//         let activity = new Activities();
+//         activity.name = req.body.name;
+//         activity.author = req.body.author;
+//         activity.activityPlace_id = activityPlace._id;
+//         activity.activitySchedule_id = activitySchedule._id;
+//
+//         let chat = new Chats();
+//         chat.title = req.body.name;
+//
+//         // TODO: add author info in such way
+//         // news.author = req.user._id;
+//
+//         activity.save(function (err) {
+//             if (err){
+//                 console.log(err);
+//                 return;
+//             }
+//             else{
+//                 chat.save(function (err) {
+//                     if (err){
+//                         console.log(err);
+//                     }
+//                 });
+//
+//                 req.flash('success', 'Activity added.');
+//                 res.redirect('/');
+//             }
+//         });
+//     }
+// });
+//
+// router.get('/:id', function (req, res) {
+//     Activities.findById(req.params.id, function (err, activity_item){
+//         if (err){
+//             console.log(err);
+//         }
+//
+//         res.render('activity',{
+//            activity_item: activity_item
+//         });
+//     });
+// });
 
-    // Get Errors
-    let errors = req.validationErrors();
-    if (errors){
-        res.render('index', {
-            title: 'Green News',
-            errors: errors
-        })
-    }else{
-        // TODO: add checking that end date is later than start date
-        let activitySchedule = new ActivitySchedule();
-        activitySchedule.start_date = req.body.start_date;
-        activitySchedule.end_date = req.body.end_date;
-
-        activitySchedule.save(function (err){
-            if (err){
-                console.log(err);
-            }
-        });
-
-        let activityPlace = new ActivityPlace();
-        activityPlace.latitude = req.body.latitude;
-        activityPlace.longitude = req.body.longitude;
-
-        activityPlace.save(function (err){
-            if (err){
-                console.log(err);
-            }
-        });
-
-        let activity = new Activities();
-        activity.name = req.body.name;
-        activity.author = req.body.author;
-        activity.activityPlace_id = activityPlace._id;
-        activity.activitySchedule_id = activitySchedule._id;
-
-        let chat = new Chats();
-        chat.title = req.body.name;
-
-        // TODO: add author info in such way
-        // news.author = req.user._id;
-
-        activity.save(function (err) {
-            if (err){
-                console.log(err);
-                return;
-            }
-            else{
-                chat.save(function (err) {
-                    if (err){
-                        console.log(err);
-                    }
-                });
-
-                req.flash('success', 'Activity added.');
-                res.redirect('/');
-            }
-        });
-    }
-});
-
-router.get('/:id', function (req, res) {
-    Activities.findById(req.params.id, function (err, activity_item){
-        if (err){
-            console.log(err);
-        }
-
-        res.render('activity',{
-           activity_item: activity_item
-        });
-    });
-});
+// API Key: 2086bbf6-3638-48d1-be92-53b9da474acf
+router.get('/list', function (req, resp){
+    resp.render('activities_list')
+})
 
 module.exports = router;
--- utils/maps.js
+++ utils/maps.js
@@ -1,94 +1,45 @@
 ymaps.ready(init);
+var myMap;
 
-function init() {
-    const latitude_Minsk = 53.893009;
-    const longitude_Minsk = 27.567444;
-
-    let myPlacemark,
-        myMap = new ymaps.Map('map', {
-            center: [latitude_Minsk, longitude_Minsk],
-            zoom: 11
-        }, {
-            searchControlProvider: 'yandex#search'
-        });
-
-
-    // Hear click on the map
-    myMap.events.add('click', function (e) {
-        let coords = e.get('coords');
-
-        // If placemark is created - move it
-        if (myPlacemark) {
-            myPlacemark.geometry.setCoordinates(coords);
-            let latitude = document.getElementById('latitude');
-            let longitude = document.getElementById('longitude');
-            latitude.value = coords[0];
-            longitude.value = coords[1];
-        }
-        // If not created - create
-        else {
-            myPlacemark = createPlacemark(coords);
-            myMap.geoObjects.add(myPlacemark);
-
-            // Hear ending of dragging event on placemark.
-            myPlacemark.events.add('dragend', function () {
-                getAddress(myPlacemark.geometry.getCoordinates());
-            });
-        }
-        getAddress(coords);
+function init () {
+    myMap = new ymaps.Map('map', {
+        center: [53.9006, 27.5590],
+        zoom: 13
+    }, {
+        searchControlProvider: 'yandex#search'
     });
 
-    // Creation of the placemark
-    function createPlacemark(coords) {
-        return new ymaps.Placemark(coords, {
-            iconCaption: 'search...'
-        }, {
-            preset: 'islands#violetDotIconWithCaption',
-            iconColor: '#3c763d',
-            draggable: true
-        });
-    }
+    // Creating a rectangle using an auxiliary class.
+    myRectangle = new ymaps.Rectangle([
+        // Setting the coordinates of the diagonal corners of the rectangle.
+        [53.9006, 27.5590],
+        [53.92, 27.59],
+    ], {
+        //Properties
+        balloonContent: 'Activity'
+    }, {
+        fillColor: 'rgba(77,140,141,0.2)',
+        //fillOpacity: 0.5,
+        strokeColor: 'rgba(77,140,141,0.2)',
+        //strokeOpacity: 0.5,
+        strokeWidth: 2,
+        borderRadius: 90
+    });
 
-    // Define address according coordinates (reverse geocoding).
-    function getAddress(coords) {
-        myPlacemark.properties.set('iconCaption', 'search...');
-        ymaps.geocode(coords).then(function (res) {
-            let firstGeoObject = res.geoObjects.get(0);
-
-            myPlacemark.properties
-                .set({
-                    // Form a string with data about object.
-                    iconCaption: [
-                        // Название населенного пункта или вышестоящее административно-территориальное образование.
-                        firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
-                        // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
-                        firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
-                    ].filter(Boolean).join(', '),
-                    // В качестве контента балуна задаем строку с адресом объекта.
-                    balloonContent: firstGeoObject.getAddressLine()
-                });
-        });
-    }
-    //
     // myMap.geoObjects
-    //     .add(new ymaps.Placemark([53.883086, 27.447377], {
-    //         balloonContent: 'Green Activity'
-    //     }, {
-    //         preset: 'islands#icon',
-    //         iconColor: '#3c763d'
-    //     }))
-    //     .add(new ymaps.Placemark([53.946016, 27.703441], {
-    //         balloonContent: 'Green Activity 2'
-    //     }, {
-    //         preset: 'islands#icon',
-    //         iconColor: '#3c763d'
-    //     }))
-    //     .add(new ymaps.Placemark([53.858224, 27.485160], {
-    //         balloonContent: 'Green Activity 3'
-    //     }, {
-    //         preset: 'islands#icon',
-    //         iconColor: '#3c763d'
-    //     }))
-}
+    //     .add(myRectangle);
 
-module.exports = init;
+    ymaps.modules.require(['Heatmap'], function (Heatmap) {
+        var data = [[53.9006, 27.5590], [53.92, 27.59]],
+            heatmap = new Heatmap(data);
+        heatmap.radius = 1000;
+        heatmap.setMap(myMap);
+    });
+
+    myMap.geoObjects
+        .add(new ymaps.Placemark([53.9040, 27.5590], {
+            balloonContent: '<strong>red</strong> color'
+        }, {
+            preset: 'islands#redSportIcon'
+        }))
+}
