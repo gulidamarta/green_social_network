bda24d62d8731d77eec8a2a9c3705db94861fc7f
Marta <43724620+gulidamarta@users.noreply.github.com>
2021-05-02 20:46:03 +0300

    Load changes to top pannel

---
+++ models/device.js
@@ -0,0 +1,51 @@
+let mongoose = require('mongoose');
+
+// Device schema
+let deviceSchema = mongoose.Schema({
+    device_id: {
+        type: Number,
+        required: true,
+    },
+    latitude: {
+        type: mongoose.Schema.Types.Decimal,
+        required: true,
+    },
+    longitude: {
+        type: mongoose.Schema.Types.Decimal,
+        required: true,
+    },
+    temperature: {
+        type: mongoose.Schema.Types.Decimal,
+        required: true,
+    },
+    humidity: {
+        type: mongoose.Schema.Types.Decimal,
+        required: true,
+    },
+    dew_point: {
+        type: mongoose.Schema.Types.Decimal,
+        required: true,
+    },
+    carbon_dioxide: {
+        type: mongoose.Schema.Types.Decimal,
+        required: true,
+    },
+    tvoc: {
+        type: mongoose.Schema.Types.Decimal,
+        required: true,
+    },
+    formaldehyde: {
+        type: mongoose.Schema.Types.Decimal,
+        required: true,
+    },
+    toluene: {
+        type: mongoose.Schema.Types.Decimal,
+        required: true,
+    },
+    cpm: {
+        type: mongoose.Schema.Types.Decimal,
+        required: true,
+    }
+});
+
+let Device = module.exports = mongoose.model('Device', deviceSchema);
